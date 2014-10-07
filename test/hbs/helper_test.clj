c500a6ab91358205e39d78fe3fb3510b85545779
Sun Ning <sunng@about.me>
2014-10-07 16:16:10 +0800

    update indent and fixed reflection warning

--- test/hbs/helper_test.clj
+++ test/hbs/helper_test.clj
@@ -1,11 +1,11 @@
 (ns hbs.helper-test
-  (:use clojure.test
-        hbs.core
-        hbs.helper))
+  (:require [clojure.test :refer :all] 
+            [hbs.core :refer :all] 
+            [hbs.helper :refer :all :exclude [hash]]))
 
 (deftest test-use-javascript-helpers
   (testing "test usage of javascript helpers"
     (is (= "bar"
-          (do
-            (register-js-helpers! "dev-resources/helpers/helpers.js")
-            (render "{{js-helper foo}}" {:foo "bar"}))))))
+           (do
+             (register-js-helpers! "dev-resources/helpers/helpers.js")
+             (render "{{js-helper foo}}" {:foo "bar"}))))))
