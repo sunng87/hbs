1c6c8b2a33ce91573236d170807a2f64043087d7
Sun Ning <sunng@about.me>
2014-10-07 16:16:33 +0800

    bump version and update handlebars.java

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject hbs "0.5.2-SNAPSHOT"
+(defproject hbs "0.6.0-SNAPSHOT"
   :description "Clojure templating by Handlebars.java"
   :url "http://github.com/sunng87/hbs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.github.jknack/handlebars "1.3.1"]]
+                 [com.github.jknack/handlebars "1.3.2"]]
   :java-source-paths ["java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :global-vars {*warn-on-reflection* true}
