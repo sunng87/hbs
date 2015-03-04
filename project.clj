0fb37f7409d0bbf2cd16748c557c77e220f700e8
Sun Ning <sunng@about.me>
2015-03-04 10:57:29 +0800

    (chore) update to Handlebars.java 2.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.github.jknack/handlebars "1.3.2"]]
+                 [com.github.jknack/handlebars "2.0.0"]]
   :java-source-paths ["java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :plugins [[codox "0.8.10"]]
