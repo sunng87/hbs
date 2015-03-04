30b4844d895d7fed899d3ba8712bb1ea02e09561
Sun Ning <sunng@about.me>
2015-03-04 10:54:41 +0800

    (test) added test for mustach style markup and j.u.HashMap as data

--- test/hbs/core_test.clj
+++ test/hbs/core_test.clj
@@ -1,15 +1,21 @@
 (ns hbs.core-test
-  (:use clojure.test
-        hbs.core))
+  (:require [clojure.test :refer :all]
+            [hbs.core :refer :all])
+  (:import [java.util HashMap]))
 
 (deftest test-render
   (testing "test inline render"
     (is (= "Hello World" (render "Hello {{person.name}}"
-                           {:person {:name "World"}})))))
+                                 {:person {:name "World"}})))))
+
+(deftest test-render-juh
+  (let [p (doto (HashMap.)
+            (.put "name" "Handlebars"))]
+    (is (= "Handlebars" (render "{{#person}}{{name}}{{/person}}"
+                                {:person p})))))
 
 (set-template-path! "/templates" ".tpl")
 
 (deftest test-render-file
   (testing "test render from file"
     (is (= "Hello World!\n" (render-file "hello" {:name "World"})))))
-
