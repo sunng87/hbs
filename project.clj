b332ceea48d88c90ba93c2aa565241adabafbe7c
Ning Sun <sunng@about.me>
2015-07-21 22:34:30 +0800

    (chore) bump dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.github.jknack/handlebars "2.0.0"]]
-  :profiles {:test {:dependencies [[ring "1.3.2"]]}}
+                 [com.github.jknack/handlebars "2.2.2"]]
+  :profiles {:test {:dependencies [[ring "1.4.0"]]}}
   :plugins [[codox "0.8.10"]]
   :codox {:output-dir "target/codox"
           :exclude [hbs.ext]}
