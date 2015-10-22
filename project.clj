80f47e22e405132389863a8c45841eac9aae5b4b
Ning Sun <sunng@about.me>
2015-10-22 10:46:23 +0800

    (chore) refresh dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/sunng87/hbs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.github.jknack/handlebars "2.2.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.github.jknack/handlebars "2.3.1"]]
   :profiles {:test {:dependencies [[ring "1.4.0"]]}}
   :plugins [[codox "0.8.10"]]
   :codox {:output-dir "target/codox"
