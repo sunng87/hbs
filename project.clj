e35e5104dbf614307ae5787d615ccd4251de624e
Ning Sun <sunng@about.me>
2015-12-11 12:05:04 +0800

    Version 0.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hbs "0.9.0-SNAPSHOT"
+(defproject hbs "0.9.0"
   :description "Clojure templating by Handlebars.java"
   :url "http://github.com/sunng87/hbs"
   :license {:name "Eclipse Public License"
