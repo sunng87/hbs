20771530eca000c2b523336ad190094291cdb7ef
Ning Sun <sunng@about.me>
2016-02-23 23:29:31 +0800

    (chore) bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject hbs "0.9.1"
+(defproject hbs "0.9.2-SNAPSHOT"
   :description "Clojure templating by Handlebars.java"
   :url "http://github.com/sunng87/hbs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [com.github.jknack/handlebars "4.0.4"]]
   :profiles {:test {:dependencies [[ring "1.4.0"]]}}
   :plugins [[codox "0.8.10"]]
