ea48dbdc4330554c1c4a837ed44303e2c4e68899
Ning Sun <sunng@about.me>
2016-02-23 23:25:54 +0800

    (chore) update dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.github.jknack/handlebars "4.0.1"]]
+                 [com.github.jknack/handlebars "4.0.4"]]
   :profiles {:test {:dependencies [[ring "1.4.0"]]}}
   :plugins [[codox "0.8.10"]]
   :codox {:output-dir "target/codox"
